4c9a6c7f473ae811746794b8783813001cc6055f
RuoyanMeng <ruoyanmeng@gmail.com>
2020-02-07 00:58:14 +0200

    improve spotify auth process

--- routes/GeniusApi.js
+++ routes/GeniusApi.js
@@ -6,68 +6,67 @@ const BASE_URL = 'https://api.genius.com'
 var env = process.env.NODE_ENV || 'genius';
 var creds = require('../credentials')[env];
 
-router.route('/fetch_lyrics').get((req,res) => {
-    let song = req.body.song.toLowerCase();
-    let artist = req.body.artist.toLowerCase();
+router.route('/fetch_lyrics').get((req, res) => {
+    let song = req.query.song.toLowerCase();
+    let artist = req.query.artist.toLowerCase();
+
+    // let song = req.body.song.toLowerCase();
+    // let artist = req.body.artist.toLowerCase();
     let lyrics_url = null
-    console.log(song+artist)
+
     axios({
-        url : '/search', 
-        baseURL : BASE_URL,
+        url: '/search',
+        baseURL: BASE_URL,
         method: 'GET',
         headers: {
             'Accept': 'application/json',
             "Authorization": "Bearer " + creds.client_access_token
         },
-        data:{
+        data: {
             "q": song + artist
         }
     })
-    .then( resp =>{
+        .then(resp => {
 
-        let results = resp.data.response.hits;
-        let rlength = results.length;
-        
-        for (let i = 0; i<rlength; i++){
+            let results = resp.data.response.hits;
+            let rlength = results.length;
 
-            let _song = results[i].result.full_title.split(' by',1)[0].toLowerCase();
-            let _artist = results[i].result.primary_artist.name.toLowerCase();
+            for (let i = 0; i < rlength; i++) {
 
-            console.log(_song+"....."+song)
-            console.log(_artist+"....."+artist)
-            if(_song==song && _artist==artist){
-                
-                lyrics_url = results[i].result.url;
-                break;
+                let _song = results[i].result.full_title.split(' by', 1)[0].toLowerCase();
+                let _artist = results[i].result.primary_artist.name.toLowerCase();
+
+                console.log(_song + "....." + song)
+                console.log(_artist + "....." + artist)
+                if (_song == song && _artist == artist) {
+
+                    lyrics_url = results[i].result.url;
+                    break;
+                }
             }
-        }
-    })
-    .then(()=>{
-        axios.get(lyrics_url)
-        .then(resq=>{
-            console.log($('.lyrics',resq.data))
-            res.send($('.lyrics',resq.data).text())
         })
-        .catch(err=>{
-            res.send(err)
+        .then(() => {
+            if (lyrics_url == null) {
+                res.send('Opps...')
+            }
+            else {
+                axios.get(lyrics_url)
+                    .then(resq => {
+                        res.send($('.lyrics', resq.data).text())
+                    })
+                    .catch(err => {
+                        res.send(err)
+                    })
+            }
+
+
         })
-    })
-    .catch( err =>{
-        res.send(err);
-    })
+        .catch(err => {
+            res.send(err);
+        })
 
 });
 
-// router.route('/web_scraping').get((req,res)=>{
-//     url = "https://genius.com/Adele-hello-lyrics"
-//     axios.get(url)
-//     .then(resq=>{
-//         console.log($('.lyrics',resq.data))
-//         res.send($('.lyrics',resq.data).text())
-//     })
-//     .catch(err=>{
-//         res.send(err)
-//     })
-// });
+
 
 module.exports = router;
--- routes/SpotifyAuth.js
+++ routes/SpotifyAuth.js
@@ -1,20 +1,46 @@
 const router = require('express').Router();
+const axios = require('axios');
 const BASE_URL = 'https://accounts.spotify.com'
 
 var env = process.env.NODE_ENV || 'spotify';
 var creds = require('../credentials')[env];
+let redirect_uri = process.env.REDIRECT_URI || "http://localhost:5000/callback"
 
 router.route('/login').get((req,res) => {
 
     // let redirect_uri = req.params.redirect_uri
-    let redirect_uri = "http://localhost:3000/"
+    
     let _url = BASE_URL + '/authorize' +
-            '?response_type=token' +
+            '?response_type=code' +
             '&client_id=' + creds.client_id +
             '&redirect_uri=' + encodeURIComponent(redirect_uri) + '&scope=' + Object.values(creds.scope).join('%20');
 
     res.redirect(_url);
 
 });
+
+router.route('/callback').get((req,res)=>{
+    let refresh_token = null
+    let code  = req.query.code || null;
+    let _url = 'https://accounts.spotify.com/api/token';
+    const params = new URLSearchParams();
+    params.append('code', code);
+    params.append('redirect_uri', redirect_uri);
+    params.append('grant_type', 'authorization_code');
+    params.append('client_id', creds.client_id);
+    params.append('client_secret', creds.client_secret);
+    axios.post(_url,params)
+    .then(resp=>{
+        refresh_token = resp.data.access_token
+        let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+        var access_token = resp.data.access_token
+        
+        res.status(200).redirect(uri + '?access_token=' + access_token)
+    })
+    .catch(err=>{
+        res.send(err)
+    })
+
+})
 
 module.exports = router;
