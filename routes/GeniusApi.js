9f189f5123e06f735ca3ea2ec61421588b9be986
RuoyanMeng <ruoyanmeng@gmail.com>
2020-02-13 22:30:58 +0200

    improve lyrics matching process

--- routes/GeniusApi.js
+++ routes/GeniusApi.js
@@ -7,16 +7,16 @@ const BASE_URL = 'https://api.genius.com'
 // var env = process.env.NODE_ENV || 'genius';
 // var creds = require('../credentials')[env];
 
-let redirect_uri = 
-  process.env.REDIRECT_URI || 
-  'http://localhost:5000/callback'
+let redirect_uri =
+    process.env.REDIRECT_URI ||
+    'http://localhost:5000/callback'
 
 router.route('/fetch_lyrics').get((req, res) => {
+    
     let song = req.query.song.toLowerCase();
     let artist = req.query.artist.toLowerCase();
 
-    // let song = req.body.song.toLowerCase();
-    // let artist = req.body.artist.toLowerCase();
+
     let lyrics_url = null
 
     axios({
@@ -28,10 +28,11 @@ router.route('/fetch_lyrics').get((req, res) => {
             "Authorization": "Bearer " + process.env.GENIUS_ACCESS_TOKEN
         },
         data: {
-            "q": song + artist
+            "q": song +' ' + artist
         }
-    })
+        })
         .then(resp => {
+
 
             let results = resp.data.response.hits;
             let rlength = results.length;
@@ -39,13 +40,12 @@ router.route('/fetch_lyrics').get((req, res) => {
             for (let i = 0; i < rlength; i++) {
 
                 let _song = results[i].result.full_title.trim().split(' by', 1)[0].toLowerCase();
-                let _artist = results[i].result.primary_artist.name.toLowerCase();
-                
-                console.log(song)
-                console.log(results[i].result.full_title.text.trim())
-                console.log(_artist + "....." + artist)
-                if (_song == song && _artist == artist) {
+                let _artist = results[i].result.primary_artist.name.toLowerCase().replace(/[^\x20-\x7E]/g, '');
 
+                // console.log(encodeURIComponent(_artist))
+
+                if (_song == song && _artist == artist) {
+                    
                     lyrics_url = results[i].result.url;
                     break;
                 }
@@ -53,12 +53,18 @@ router.route('/fetch_lyrics').get((req, res) => {
         })
         .then(() => {
             if (lyrics_url == null || lyrics_url == {}) {
-                res.send('Opps...')
+                res.send('Oops! No results found')
             }
             else {
                 axios.get(lyrics_url)
                     .then(resq => {
-                        res.send($('.lyrics', resq.data).text())
+                        let lyrics = $('.lyrics', resq.data).text()
+                        if (JSON.stringify(lyrics) === '{}') {
+                            res.send('Oops! No results found')
+                        } else {
+                            res.send(lyrics)
+                        }
+
                     })
                     .catch(err => {
                         res.send(err)
